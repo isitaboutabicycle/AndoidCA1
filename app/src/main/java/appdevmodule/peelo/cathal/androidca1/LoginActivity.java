7ea8406601d547c875cd63083e9c09e53b09f54c
isitaboutabicycle <31797364+isitaboutabicycle@users.noreply.github.com>
2017-09-27 17:23:44 +0100

    Added a register activity

--- app/src/main/java/appdevmodule/peelo/cathal/androidca1/LoginActivity.java
+++ app/src/main/java/appdevmodule/peelo/cathal/androidca1/LoginActivity.java
@@ -102,6 +102,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });*/
 
+        Button mRegisterButton = (Button) findViewById(R.id.register_button);
+
+        mRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerActivityIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+
+                startActivity(registerActivityIntent);
+            }
+        });
+
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
---
+++ app/src/main/java/appdevmodule/peelo/cathal/androidca1/RegisterActivity.java
@@ -0,0 +1,13 @@
+package appdevmodule.peelo.cathal.androidca1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
