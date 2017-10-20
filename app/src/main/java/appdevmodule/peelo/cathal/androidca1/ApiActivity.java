c4b578390bc4689c597b3218807309d7bdff2e4a
isitaboutabicycle <31797364+isitaboutabicycle@users.noreply.github.com>
2017-10-20 23:22:08 +0100

    API Activity now dumps JSON to a TextView. Does not work on emulators due to the date.

--- app/src/main/java/appdevmodule/peelo/cathal/androidca1/ApiActivity.java
+++ app/src/main/java/appdevmodule/peelo/cathal/androidca1/ApiActivity.java
@@ -11,14 +11,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 import static appdevmodule.peelo.cathal.androidca1.R.id.progressBar;
 import static appdevmodule.peelo.cathal.androidca1.R.id.queryButton;
@@ -27,16 +31,22 @@ import static appdevmodule.peelo.cathal.androidca1.R.id.responseView;
 public class ApiActivity extends AppCompatActivity {
 
     int feedCode = 1;
+    TextView readout = null;
+    ArrayList bikes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_api);
+
+        readout = (TextView) findViewById(R.id.responseView);
+        bikes = new ArrayList<>();
+
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
         //RetrieveFeedTask taskThingumy = new RetrieveFeedTask();
-        Button doTheThing = (Button) findViewById(queryButton);
+        Button doTheThing = (Button) findViewById(R.id.queryButton);
 
         doTheThing.setOnClickListener(new View.OnClickListener() {
 
@@ -48,7 +58,8 @@ public class ApiActivity extends AppCompatActivity {
 
                 RetrieveFeedTask goldenRetriever = new RetrieveFeedTask();
 
-                goldenRetriever.onPreExecute();
+                //goldenRetriever.onPreExecute();
+                goldenRetriever.execute();//from helper
                 /* for parsing JSON into an object
 
                  try {
@@ -65,39 +76,61 @@ public class ApiActivity extends AppCompatActivity {
                  }
 
                  */
-                String readout = goldenRetriever.doInBackground();
-                TextView readoutHolder = (TextView) findViewById(R.id.responseView);
-                readoutHolder.setText(readout);
-                goldenRetriever.onPostExecute(readout);
+                //String json = goldenRetriever.doInBackground();
+                //setReadout(json);
+                //goldenRetriever.onPostExecute(json);
             }
         });
 
     }
 
+    private void setReadout(String filling){
+        if (readout != null){
+            readout.setText(filling);
+        }
+    }
+
+
+
     class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
 
-        private Exception exception;
-        TextView readout = (TextView) findViewById(responseView);
+        //private Exception exception;
+        //String json = null;
 
         protected void onPreExecute() {
+            super.onPreExecute();//from helper
             findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
-            this.readout.setText("");
+            setReadout("");
         }
 
         protected String doInBackground(Void... urls) {
 
+            String response = null;
+
             try {
                 URL url = new URL("https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=fcd108bd549949a9d7ee003a683cdece0fea1ff2");
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+
                 try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    InputStream myIS = urlConnection.getInputStream();
+                    //InputStreamReader myISReader = new InputStreamReader(myIS);
+                    //BufferedReader bufferedReader = new BufferedReader(myISReader);
+                    InputStream myBIS = new BufferedInputStream(myIS);
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(myBIS));
                     StringBuilder stringBuilder = new StringBuilder();
+
+                    //parsing the response
                     String line;
                     while ((line = bufferedReader.readLine()) != null) {
                         stringBuilder.append(line).append("\n");
                     }
                     bufferedReader.close();
-                    return stringBuilder.toString();
+                    response = stringBuilder.toString();
+
+                    //if (response != null)
+
                 }
                 finally{
                     urlConnection.disconnect();
@@ -107,6 +140,8 @@ public class ApiActivity extends AppCompatActivity {
                 Log.e("ERROR", e.getMessage(), e);
                 return e.getMessage();//changed from null
             }
+
+            return response;
         }
 
         protected void onPostExecute(String response) {
@@ -114,8 +149,9 @@ public class ApiActivity extends AppCompatActivity {
                 response = "Oops, that didn't go to plan";
             }
             findViewById(R.id.progressBar).setVisibility(View.GONE);
+
             Log.i("INFO", response);
-            readout.setText(response);
+            setReadout(response);
         }
     }
 
